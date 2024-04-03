bfd9047226ef173901a4046671b265928615f79a
João Pedro <89290727+Jaunzera01@users.noreply.github.com>
2024-04-03 08:43:36 -0300

    gy

--- cont.js
+++ cont.js
@@ -2,9 +2,12 @@ const botoes = document.querySelectorAll('.objetivo');
 //botoes.length
 
 let i = 0;
-
-
-
 for(let i = 0; i < botoes.length; i++ ){
 console.log(i);
+botoes[i].onlinck = function(){
+ botoes[i].classList.add('ativo');
+
+}
+
+ // botoes[i].classList.remove('ativo);
 }
