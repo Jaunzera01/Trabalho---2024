685792c63e04b4d69b49fa456f64977327941b23
João Pedro <89290727+Jaunzera01@users.noreply.github.com>
2024-04-03 08:21:21 -0300

    dj

--- cont.js
+++ cont.js
@@ -1,1 +1,10 @@
-const botoes = document.querySelectorAll('.objetivo')
+const botoes = document.querySelectorAll('.objetivo');
+//botoes.length
+
+let i = 0;
+
+
+
+for(let i = 0; i < botoes.length; i++ ){
+console.log(i)
+}
