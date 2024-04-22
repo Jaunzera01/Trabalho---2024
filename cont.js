21d72bbb7064a1dca36f4ce3fe6d6e0d7fbbe7e6
João Pedro <89290727+Jaunzera01@users.noreply.github.com>
2024-04-22 11:27:24 -0300

    dtrxfyguhi

--- cont.js
+++ cont.js
@@ -11,3 +11,16 @@ botoes[i].onlinck = function(){
 
  // botoes[i].classList.remove('ativo);
 }
+
+
+const contadores = document.querySelectorAll('.contador');
+const tempoObjetivo1 = new Date ('2024-04-17T00:00:00');
+
+let TempoAtual = new Date ();
+let tempoFinal = tempoObjetivo1 - TempoAtual;
+let segundos = Math.floor( tempoFinal/1000);]
+let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
+
+contadores[0].textContent = dias + "dias" + horas + "horas" + minutos +
